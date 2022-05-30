c06526d6243a6dfa4368bab4672dbf32ed834d90
TurtleException <turtle.exception@eldritch.de>
2022-05-30 16:00:02 +0200

    Initial commit

---
+++ src/main/java/de/eldritch/spigot/poppy_hats/PoppyHats.java
@@ -0,0 +1,10 @@
+package de.eldritch.spigot.poppy_hats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PoppyHats extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+}
